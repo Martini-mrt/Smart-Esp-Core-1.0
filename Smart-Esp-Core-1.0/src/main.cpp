3674f012256777cc1fc841bf9507261e639b43c9
Artem Morgunov <martinidance@mail.ru>
2023-11-22 00:46:26 +0300

    working wifi

---
+++ Smart-Esp-Core-1.0/src/main.cpp
@@ -0,0 +1,61 @@
+#include <Arduino.h>
+
+#include <ESP8266WiFi.h>
+// #include <WiFiClient.h>
+
+const char *wifiSSID = "Orang";
+const char *wifiPASS = "lenina2431";
+
+WiFiEventHandler gotIpEventHandler, disconnectedEventHandler;
+
+bool ledState;
+
+void setup()
+{
+
+  Serial.begin(9600);
+
+  pinMode(D1, OUTPUT);
+
+  Serial.println();
+  Serial.println("DEMO");
+
+  gotIpEventHandler = WiFi.onStationModeGotIP([](const WiFiEventStationModeGotIP &event)
+                                              {
+    Serial.print("Station connected, IP: ");
+    Serial.println(WiFi.localIP()); });
+
+  disconnectedEventHandler =
+   WiFi.onStationModeDisconnected([](const WiFiEventStationModeDisconnected &event)
+                                                            {
+                                                              Serial.println("Station disconnected");
+                                                            });
+
+  Serial.printf("Connecting to %s ...\n", wifiSSID);
+  WiFi.begin(wifiSSID, wifiPASS);
+
+  // WiFi.mode(WIFI_AP_STA);
+  // WiFi.begin(wifiSSID, wifiPASS);
+
+  // Serial.print("Connections to Wi-Fi AP ");
+  // Serial.print(wifiSSID);
+  // Serial.print(" ");
+
+  // while (WiFi.status() != WL_CONNECTED)
+  // {
+  //   Serial.print(".");
+  //   delay(500);
+  // };
+
+  // Serial.println(" ok");
+  // Serial.print("WiFi connected, obtained IP address: ");
+  // Serial.print(WiFi.localIP());
+}
+
+void loop()
+{
+
+  digitalWrite(D1, ledState);
+  ledState = !ledState;
+  delay(250);
+}
