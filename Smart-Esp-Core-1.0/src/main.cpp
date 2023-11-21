ffa72a4957b912912feb9500f90c4a25d614c4cc
Artem Morgunov <martinidance@mail.ru>
2023-11-22 01:20:39 +0300

    fix wifi

--- Smart-Esp-Core-1.0/src/main.cpp
+++ Smart-Esp-Core-1.0/src/main.cpp
@@ -23,7 +23,13 @@ void setup()
   gotIpEventHandler = WiFi.onStationModeGotIP([](const WiFiEventStationModeGotIP &event)
                                               {
     Serial.print("Station connected, IP: ");
-    Serial.println(WiFi.localIP()); });
+    Serial.println(WiFi.localIP()); 
+    
+    // выводим диагностическую инфу про сеть
+    WiFi.printDiag(Serial);
+    // Serial.println("Prohod 2222");
+    
+    });
 
   disconnectedEventHandler =
    WiFi.onStationModeDisconnected([](const WiFiEventStationModeDisconnected &event)
@@ -34,9 +40,15 @@ void setup()
   Serial.printf("Connecting to %s ...\n", wifiSSID);
   WiFi.begin(wifiSSID, wifiPASS);
 
+
+
+
+
+//  Serial.println("Prohod");
+  
+
   // WiFi.mode(WIFI_AP_STA);
   // WiFi.begin(wifiSSID, wifiPASS);
-
   // Serial.print("Connections to Wi-Fi AP ");
   // Serial.print(wifiSSID);
   // Serial.print(" ");
@@ -54,7 +66,7 @@ void setup()
 
 void loop()
 {
-
+   
   digitalWrite(D1, ledState);
   ledState = !ledState;
   delay(250);
